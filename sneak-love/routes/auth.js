737b4a09c1c0d61ed9c6b8c042ab6d58289e2616
sarah ledu <sarah.m.ledu@gmail.com>
2019-11-14 11:01:40 +0100

    modif routes

--- sneak-love/routes/auth.js
+++ sneak-love/routes/auth.js
@@ -1,3 +1,13 @@
 const express = require("express");
 const router = new express.Router();
+
+
+router.get("/signup", (req, res) => {
+    res.send("sneak");
+  });
+  
+  router.get("/signin", (req, res) => {
+    res.send("love");
+  });
+  
 module.exports = router;
--- sneak-love/routes/index.js
+++ sneak-love/routes/index.js
@@ -13,13 +13,5 @@ router.get("/one-product/:id", (req, res) => {
   res.send("baz");
 });
 
-router.get("/signup", (req, res) => {
-  res.send("sneak");
-});
-
-router.get("/signin", (req, res) => {
-  res.send("love");
-});
-
 
 module.exports = router;

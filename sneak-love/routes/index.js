0060c52bc72d3b8a52a029d31f3aba8d1f06ff7e
Parina K <Parina.khennavong@gmail.com>
2019-11-15 14:25:47 +0100

    test

--- sneak-love/routes/index.js
+++ sneak-love/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const sneakerModel = require("../models/Sneaker");
+const tagModel = require("../models/Tag");
 
 router.get("/", (req, res) => {
   res.render("index");
@@ -17,7 +18,15 @@ router.get("/sneakers/:cat", (req, res) => {
 router.get("/one-product/:id", (req, res) => {
   sneakerModel
     .findById(req.params.id)
-    .then(dbRes => res.render("one_product", { sneakers: dbRes }))
+    .then(dbRes => res.render("one_product", { sneaker: dbRes }))
+    .catch(err => console.log(err));
+});
+
+router.get("/all-sneakers", (req, res) => {
+  // const label = [];
+  tagModel
+    .find()
+    .then(dbRes => res.render("products", { tags: dbRes }))
     .catch(err => console.log(err));
 });
 

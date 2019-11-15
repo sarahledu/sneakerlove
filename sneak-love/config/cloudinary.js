410b2a6c52b6d4d4785d9f2477e4f1726d28db14
sarah ledu <sarah.m.ledu@gmail.com>
2019-11-15 11:54:16 +0100

    "image upload done"

--- sneak-love/config/cloudinary.js
+++ sneak-love/config/cloudinary.js
@@ -2,6 +2,7 @@
 const cloudinary = require("cloudinary");
 const cloudinaryStorage = require("multer-storage-cloudinary");
 const multer = require("multer");
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_KEY,
